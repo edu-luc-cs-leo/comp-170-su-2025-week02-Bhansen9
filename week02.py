d87b1d4edf0a646561db59fa03583f57ece3a0c9
Ben Hansen <Benjamin.t.hansen@icloud.com>
2025-06-05 22:02:20 +0000

    1 and 2 done

--- week02.py
+++ week02.py
@@ -1,1 +1,22 @@
-# WRITE YOUR CODE HERE
+
+def greeting_function():
+    name = input("What is your name: ")
+    print("Hello",name,". How are you?")
+#greeting_function()
+
+
+
+
+def Greet_Friends(name, greeting):
+    print("Hello "+ name +",")
+    print(greeting)
+    print()
+
+my_friends = {
+    "Ryan": "Its nice to see you.",
+    "Henry": "Its nice to see you.",
+    "Roise": "Its nice to see you."
+}
+
+for name, greeting in my_friends.items():
+    Greet_Friends(name, greeting)
